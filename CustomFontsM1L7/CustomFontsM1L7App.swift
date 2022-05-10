b73ee0f335eef1df039fb19046e5e29984dbf45e
agholson <87455899+agholson@users.noreply.github.com>
2022-05-09 21:12:16 -0400

    Initial Commit

---
+++ CustomFontsM1L7/CustomFontsM1L7App.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFontsM1L7App.swift
+//  CustomFontsM1L7
+//
+//  Created by Leone on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomFontsM1L7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
